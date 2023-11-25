953103830a9d90a8d1eb5f179ee1e21828ca6e0f
Kade Allen <kadeallenprofessional@gmail.com>
2023-11-25 21:58:42 +1300

    First initial version

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        vulcan: {
+          "50": "#acbef1",
+          "100": "#a1b5ed",
+          "200": "#7ca0e9",
+          "300": "#477ae1",
+          "400": "#2259bf",
+          "500": "#1b4188",
+          "600": "#0f2b66",
+          "700": "#0b1e46",
+          "800": "#09152f",
+          "900": "#081021",
+          "950": "#060b19",
+        },
+      },
     },
   },
   plugins: [],
